bfcdbc55a810c7ba8b8c6d0593970f400290b320
haxscramper <haxscramper@gmail.com>
2020-08-20 19:03:33 +0300

    [TEST] Check parsing inside of `static:` block too
    Changed:
      - TEST check `LL(*)` parser generator in both static and runtime
        context.

--- tests/tSimpleParsers.nim
+++ tests/tSimpleParsers.nim
@@ -26,27 +26,35 @@ proc tokenize(
 
 import unittest
 
+template staticAndRuntime(body: untyped): untyped =
+  static:
+    body
+
+  body
+
 suite "Simple parsers":
   test "Nested tags":
-    const defaultCategory = tpPunct
+    staticAndRuntime: # Check both runtime and compile-time parsing at
+                      # once.
+      const defaultCategory = tpPunct
 
-    let tokens = "#eee##[ee,ee]".tokenize({
-      {'#'} : tpPunct,
-      {'['} : tpPunct,
-      {']'} : tpPunct,
-      {','} : tpPunct,
-      {'a'..'z', 'A'..'Z', '0'..'9'} : tpIdent
-    }).mapIt:
-      makeTokenNoInfo(it[0], it[1])
+      let tokens = "#eee##[ee,ee]".tokenize({
+        {'#'} : tpPunct,
+        {'['} : tpPunct,
+        {']'} : tpPunct,
+        {','} : tpPunct,
+        {'a'..'z', 'A'..'Z', '0'..'9'} : tpIdent
+      }).mapIt:
+        makeTokenNoInfo(it[0], it[1])
 
 
-    initGrammarConst[TagParts, string](grammar):
-      Tag ::= !"#" & ident & @*(@(!"##" & Subtag))
-      Subtag ::= (!"[" & ident & @*(@(!"," & ident)) & !"]") | ident
+      initGrammarConst[TagParts, string](grammar):
+        Tag ::= !"#" & ident & @*(@(!"##" & Subtag))
+        Subtag ::= (!"[" & ident & @*(@(!"," & ident)) & !"]") | ident
 
-    let parser = newLLStarParser[TagParts, string, void](grammar)
+      let parser = newLLStarParser[TagParts, string, void](grammar)
 
-    var toks = tokens.makeStream()
+      var toks = tokens.makeStream()
 
-    let tree = parser.parse(toks)
-    echo tree.treeRepr()
+      let tree = parser.parse(toks)
+      echo tree.treeRepr()
