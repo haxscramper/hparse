cab4e2818c609756b8d72bb0c7d943d33b2ff065
haxscramper <haxscramper@gmail.com>
2020-10-13 19:58:11 +0300

    [FEATURE] Parser example

--- src/hparse/htreesitter/parser_user.nim
+++ src/hparse/htreesitter/parser_user.nim
@@ -1,4 +1,12 @@
 import cppwrapper, macros
+import hmisc/macros/matching
+
+{.experimental: "caseStmtMacros".}
+
+{.passl: "-lstdc++".}
+{.passl: "-ltree-sitter".}
+{.passl: "cppscanner.o".}
+{.passl: "cppparser.o".}
 
 var parser = newCppParser()
 
@@ -9,3 +17,10 @@ int a = 12;
 let tree = parser.parseString(str)
 
 echo tree.treeRepr(str)
+
+proc strVal(node: CppNode, str: string): string =
+  str[node.slice()]
+
+case tree[0]:
+  of Declaration[@dtype, .._]:
+    echo "first is declaration with type ", dtype.strVal(str)
